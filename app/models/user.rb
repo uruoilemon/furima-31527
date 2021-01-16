6897be7ea8f855a56b196b034df0b43e565d2430
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-01-16 19:37:00 +0900

    Rubocopを実行し、自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'is invalid. Input full-width characters.' } do
-    validates :last_name
-    validates :first_name
+      validates :last_name
+      validates :first_name
     end
     with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' } do
-    validates :last_name_kana
-    validates :first_name_kana
+      validates :last_name_kana
+      validates :first_name_kana
     end
     validates :birthday
   end
