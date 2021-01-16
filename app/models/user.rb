9f6a191b7b82e7d367f37e19d5d3c5eacc1d5743
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-01-16 18:56:25 +0900

    バリデーション修正「々」

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' } do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'is invalid. Input full-width characters.' } do
     validates :last_name
     validates :first_name
     end
