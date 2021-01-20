f729a710720bf8e2cf4bdecc290cc0f40843a548
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-01-20 14:21:06 +0900

    プルダウン形式での表示実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,19 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
+  belongs_to :payment
+  belongs_to :prefecture
+  belongs_to :delivery_day
 
   validates :name, :string, presence: true
 
-  validates :category_id, numericality: { other_than: 1 } 
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :payment_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
 
 end
