7627c6b2063f3bf0982633b7ebf8d2d68f4516e7
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-01-20 17:06:06 +0900

    itemsコントローラー new,createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
   def new
+    @item = Item.new
   end
+
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:text,:category_id,:delivery_id,:payment_id,:prefecture_id,:status_id)
+  end
+
 end
