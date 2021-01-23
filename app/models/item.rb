2ba78d968052def76fcc779cf266e054a4c57b0c
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-01-23 18:35:30 +0900

    入力金額にバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,14 @@ class Item < ApplicationRecord
 
   with_options presence: true do
   validates :name
+  validates :text
   validates :image
-  validates :price
   end
   
-  with_options numericality: { other_than: 1 } do
+  validates :price, numericality:{ with:/\A[0-9]+\z/, message:"Half-width number"}
+  validates :price, numericality:{greater_than_or_equal_to:300, less_than_or_equal_to:9999999, message: 'Out of setting range' }
+  
+  with_options numericality: { other_than: 1, message: 'Select'} do
     validates :category_id
     validates :status_id
     validates :payment_id
