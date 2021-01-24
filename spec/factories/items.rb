ca472ec1e6596a58f91f2eaf15a289dcceeb83e2
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-01-24 21:29:07 +0900

    itemモデルの単体テストコード実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Lorem.sentence }
+    text { Faker::Lorem.sentence }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
+    association :user
+    association :category
+    association :status
+    association :payment
+    association :prefecture
+    association :delivery_day
   end
 end
