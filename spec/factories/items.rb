d8c032759c6d55cd46c012c136dc83e7c72127c8
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-01-25 18:42:25 +0900

    FactoryBotの記述修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,16 +3,14 @@ FactoryBot.define do
     name { Faker::Lorem.sentence }
     text { Faker::Lorem.sentence }
     price { Faker::Number.between(from: 300, to: 9_999_999) }
-
+    category_id { 2 }
+    status_id { 2 }
+    payment_id { 2 }
+    prefecture_id { 2 }
+    delivery_day_id { 2 }
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
-
     association :user
-    association :category
-    association :status
-    association :payment
-    association :prefecture
-    association :delivery_day
   end
 end
