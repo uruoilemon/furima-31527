090f2699208718846495bf0e6145babb366702fe
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-01-30 14:39:28 +0900

    記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,11 +21,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-  end
+  #def edit
+  #end
 
-  def destroy
-  end
+  #def destroy
+  #end
 
   private
 
