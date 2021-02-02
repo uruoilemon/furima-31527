c811835a758f0c0e4f6e26016a173e9b7aca9f4a
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-02-02 19:45:58 +0900

    destroyアクションを追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -34,8 +34,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  # end
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
   private
 
