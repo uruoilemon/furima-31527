9b597066efcda5c50fe1667fd5a145238b76562c
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-02-13 15:56:35 +0900

    各modelにassociation記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_day
   has_one_attached :image
+  has_one :order
 
   with_options presence: true do
     validates :name
