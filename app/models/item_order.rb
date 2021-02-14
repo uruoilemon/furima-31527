1f29563080b93582111d3b767381c0500c7f0e94
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-02-14 19:06:19 +0900

    Update item_order.rb

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,12 +1,13 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :item_id, :user_id, :token, :price
   
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :city
     validates :address
     validates :phone_number,format: { with: /\A\d{10,11}\z/, message: "ten or elven digits"}
+    validates :token
   end
 
   validates :prefecture, numericality: { other_than: 1, message: "Select" }
