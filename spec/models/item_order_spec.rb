b4a7e18efc24c09b757bdc4c82caa8bb2b3df421
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-02-15 23:00:49 +0900

    Rspecテストコード修正

--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe ItemOrder, type: :model do
         expect(@item_order.errors.full_messages).to include "Postal code can't be blank"
       end
       it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できない' do
-        @item_order.postal_code = 12_345_678
+        @item_order.postal_code = "12_345_678"
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include 'Postal code is invalid. Include hyphen(-)'
       end
@@ -47,14 +47,29 @@ RSpec.describe ItemOrder, type: :model do
         expect(@item_order.errors.full_messages).to include "Phone number can't be blank"
       end
       it 'phone_numberが10桁未満では保存できない' do
-        @item_order.phone_number = 123_456_789
+        @item_order.phone_number = "123456789"
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include 'Phone number ten or elven digits'
+        expect(@item_order.errors.full_messages).to include 'Phone number is only numbers. And ten or elven digits'
+      end
+      it 'phone_numberが英数混合では登録できない' do
+        @item_order.phone_number = "0901234567a"
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include 'Phone number is only numbers. And ten or elven digits'
       end
       it 'tokenが空では保存できない' do
         @item_order.token = nil
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include "Token can't be blank"
+      end
+      it 'user_idが空では保存できない' do
+        @item_order.user_id = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include "User can't be blank"
+      end
+      it 'item_idが空では保存できない' do
+        @item_order.item_id = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include "Item can't be blank"
       end
     end
   end
