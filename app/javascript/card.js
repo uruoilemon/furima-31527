77bddd1feff578f72d98a135419bc7a3be37442d
uruoilemon <poitto_banana31@yahoo.co.jp>
2021-02-15 22:59:20 +0900

    デバック用の不要な記述を削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,7 +13,6 @@ const pay = () => {
       exp_month: formData.get("item_order[exp_month]"),
       exp_year: `20${formData.get("item_order[exp_year]")}`,
     };
-    console.log(card);
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
